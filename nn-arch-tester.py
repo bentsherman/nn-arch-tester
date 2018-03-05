bd41822aff25191516b5ab7d544a137888f05488
Ben Shealy <bentshermann@gmail.com>
2018-03-05 17:26:05 -0500

    Changed kfold to be correct

--- nn-arch-tester.py
+++ nn-arch-tester.py
@@ -17,33 +17,28 @@ def evaluate(X_train, y_train, X_test, y_test, sizes, num_iter=1000, batch_size=
 	return net.evaluate(X_test, y_test)
 
 def kfold(X, y, sizes, num_folds=5):
-	n = X.shape[0]
-	n_train = n * (num_folds - 1) / num_folds
-	n_test = n - n_train
-	indices = random.sample(xrange(n), n)
+	X_folds = np.split(X, num_folds)
+	y_folds = np.split(y, num_folds)
 	scores = []
 
 	for i in xrange(num_folds):
-		X_train = X[indices[0:n_train]]
-		y_train = y[indices[0:n_train]]
-		X_test = X[indices[n_train:]]
-		y_test = y[indices[n_train:]]
+		X_train = np.concatenate(X_folds[:i] + X_folds[i+1:])
+		y_train = np.concatenate(y_folds[:i] + y_folds[i+1:])
+		X_test = X_folds[i]
+		y_test = y_folds[i]
 
 		scores.append(evaluate(X_train, y_train, X_test, y_test, sizes))
 
 	return scores
 
 # initialize data
-X_train = mnist.train_images
-y_train = to_categorical(mnist.train_labels)
-
-X_test = mnist.test_images
-y_test = to_categorical(mnist.test_labels)
+X = mnist.train_images
+y = to_categorical(mnist.train_labels)
 
 # initialize hyperparameters
-layers = [X_train.shape[1], 10]
+layers = [X.shape[1], 10]
 
 # run experiments
-scores = kfold(X_train, y_train, layers)
+scores = kfold(X, y, layers)
 
 print "%.3f +/- %.3f" % (np.mean(scores), np.std(scores))
